fc6d2dda5b4883f376322f2b1ddb6903a7722691
cylin <cylin.cs@gmail.com>
2020-04-14 21:03:19 +0800

    Get location of device by ip

--- app/src/main/java/com/ck19/infodroid/UsersActivity.java
+++ app/src/main/java/com/ck19/infodroid/UsersActivity.java
@@ -2,15 +2,21 @@ package com.ck19.infodroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -46,18 +52,64 @@ public class UsersActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                results.put("IP", response.body().string());
-                adapter.notifyDataSetChanged();
+            public void onResponse(@NotNull Call call, @NotNull final Response response) throws IOException {
+                String ip = response.body().string();
+                results.put("IP", ip);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String ip = (String) results.get("IP");
+                        results.remove("IP");
+//                        adapter.notifyDataSetChanged();
+                        getLocation((String) ip);
+                    }
+                });
             }
         });
+    }
+
+    private void getLocation(String ip) {
+        String url = "http://ip-api.com/json/" + ip + "?lang=zh-CN";
+        get(url, new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.e(TAG, "onFailure: " + e.getLocalizedMessage());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                String jsonStr = response.body().string();
+                try {
+                    JSONObject json = new JSONObject(jsonStr);
+                    String country = json.getString("country");
+                    String region = json.getString("regionName");
+                    String city = json.getString("city");
+                    results.put("Location", country + " " + region + " " + city);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (JSONException exception) {
+                    Log.e(TAG, "onResponse: " + exception.getLocalizedMessage());
+                }
+            }
+        });
+    }
+
+    private void getStack() {
+        ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcessInfos = activityManager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo info: appProcessInfos) {
+            Log.i(TAG, "getStack: " + info.processName);
+        }
     }
 
     private HashMap<String, Object> getInformation() {
         // Object can be String or String[]
         // results.put(key, value);
         // ...
-        Log.i(TAG, "getInformation: " + results);
         return results;
     }
 
@@ -66,10 +118,12 @@ public class UsersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
 
+        getInformation();
         adapter = new InformationAdapter(UsersActivity.this, results);
         ListView listView = findViewById(R.id.list_system);
         listView.setAdapter(adapter);
 
         getIp();
+        getStack();
     }
 }
