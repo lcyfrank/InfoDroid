66c8c39bef7fea801a86a676b1d1b61106bbda9f
Zero0one1 <arya_lee@qq.com>
2020-04-17 14:41:06 +0800

    get applist

--- app/src/main/java/com/ck19/infodroid/AppsActivity.java
+++ app/src/main/java/com/ck19/infodroid/AppsActivity.java
@@ -2,7 +2,11 @@ package com.ck19.infodroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import java.util.HashMap;
@@ -16,8 +20,33 @@ public class AppsActivity extends AppCompatActivity {
         // Object can be String or String[]
         // results.put(key, value);
         // ...
+        getPackageList(this);
 
         return results;
+    }
+
+    private void getPackageList(Context ctx) {
+        PackageManager packManager = ctx.getPackageManager();
+        String[] appUid = null;
+        int uid = 1000;
+        while (uid <= 19999) {
+            appUid = packManager.getPackagesForUid(uid);
+            if (appUid != null && appUid.length > 0) {
+                for (String item : appUid) {
+                    try {
+                        final PackageInfo packageInfo = packManager.getPackageInfo(item, 0);
+                        if (packageInfo == null) {
+                            break;
+                        }
+                        CharSequence applicationLabel = packManager.getApplicationLabel(packManager.getApplicationInfo(packageInfo.packageName, PackageManager.GET_META_DATA));
+                        results.put(applicationLabel.toString(), packageInfo.packageName);
+                    } catch (PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            uid++;
+        }
     }
 
     @Override
